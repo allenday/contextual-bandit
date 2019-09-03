0772a14bec364841c488fade1a216b4cbb871e3a
Allen Day <allenday@allenday.com>
2019-09-03 18:58:17 +0800

    add docs, remove unused methods

--- DataGenerator.py
+++ DataGenerator.py
@@ -2,11 +2,12 @@ import numpy as np
 
 class DataGenerator():
     """
-    Generate badit data.
+    Generate bandit data.
 
     Defaults:
-    K=2 arms
-    D=2 features/arm
+    K = number of arms (default 2)
+    D = number of features per arm (default 2)
+    reward_type = one of: binary, positive, mixed
     """
     def __init__(self,K=2,D=2,feature_type='binary',reward_type='binary'):
         
@@ -14,17 +15,38 @@ class DataGenerator():
         self.K = K # number of bandits
         self.reward_type = reward_type
         self.feature_type = feature_type
+        #initialize vectors of size K...
+        #...representing the mean expected payouts for the arms
         self.means = np.random.normal(size=self.K)
+        #...and the corresponding standard deviations of the expected payouts for the arms
         self.stds = 1 + 2*np.random.rand(self.K)
         
-        # generate the weight vectors.  initialize estimate of feature
-        # importance for each arm's d features
         self.generate_weight_vectors()
         
+    """
+    Generate weight matrix. Initialized randomly.
+
+    For K arms and D features, creae a matrix of size (K,D).
+    Any given cell (Ki,Dj) represents the magnitude that feature
+    Dj has on the probability that arm Ki is the optimal choice.
+    """
     def generate_weight_vectors(self,loc=0.0,scale=1.0):
         self.W = np.random.normal(loc=loc,scale=scale,size=(self.K,self.D))
         #self.W = np.ones((self.K,self.D))
 
+    """
+    Generate N observations.
+
+    Returns two arrays:
+    - X: N feature vectors of size D for the observation
+    - R: N reward vectors of size K corresponding to observations X
+
+    Note that this is a *data generator* and has complete information
+    about the reward of each arm in K. These data are used by the simulator
+    to choose its best guess of a single arm, then we can calculate the
+    amount of "regret" (magnitude of lost opportunity) if a suboptimal choice
+    was made.
+    """
     def generate_samples(self,n=1000):
         # the sample feature vectors X are only binary
 
@@ -48,49 +70,3 @@ class DataGenerator():
             R *= np.random.lognormal(self.means + IP,self.stds)
 
         return X,R
-
-    # generate all bernoulli rewards ahead of time
-    def generate_bernoulli_bandit_data(self,num_samples):
-        #initialize parameter estimates
-        CTRs_that_generated_data = np.tile(np.random.rand(self.K),(num_samples,1))
-        #did the trial succeed?
-        true_rewards = np.random.rand(num_samples,self.K) < CTRs_that_generated_data
-        return true_rewards,CTRs_that_generated_data
-
-    # Thompson Sampling
-    # basic idea: samples from distribution and compares those values for the arms instead
-    # http://www.economics.uci.edu/~ivan/asmb.874.pdf
-    # http://camdp.com/blogs/multi-armed-bandits
-    def thompson_sampling(self,observed_data):
-        return np.argmax( np.random.beta(observed_data[:,0], observed_data[:,1]) )
-
-
-    # the bandit algorithm
-    def run_bandit_alg(self,true_rewards,CTRs_that_generated_data,choice_func):
-        num_samples,K = true_rewards.shape
-        observed_data = np.zeros((K,2))
-        # seed the estimated params
-        prior_a = 1. # aka successes
-        prior_b = 1. # aka failures
-        observed_data[:,0] += prior_a # allocating the initial conditions
-        observed_data[:,1] += prior_b
-        regret = np.zeros(num_samples)
-
-        for i in range(0,num_samples):
-            # pulling a lever & updating observed_data
-            this_choice = choice_func(observed_data)
-
-            # update parameters
-            if true_rewards[i,this_choice] == 1:
-                update_ind = 0
-            else:
-                update_ind = 1
-
-            observed_data[this_choice,update_ind] += 1
-
-            # updated expected regret
-            regret[i] = np.max(CTRs_that_generated_data[i,:]) - CTRs_that_generated_data[i,this_choice]
-
-        cum_regret = np.cumsum(regret)
-
-        return cum_regret
