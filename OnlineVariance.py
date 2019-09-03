c4589f236c82349b9cae41b05bf3f0267bfeeaae
Allen Day <allenday@allenday.com>
2019-09-03 19:07:15 +0800

    docs!

--- OnlineVariance.py
+++ OnlineVariance.py
@@ -1,16 +1,25 @@
 import numpy as np
-#http://stackoverflow.com/questions/5543651/computing-standard-deviation-in-a-stream
-class OnlineVariance(object):
-    """
-    Welford's algorithm computes the sample variance incrementally.
-    """
+"""
+Descriptive statistics refactored as O(n).
 
+Welford's algorithm computes the sample variance incrementally.
+
+See also: http://stackoverflow.com/questions/5543651/computing-standard-deviation-in-a-stream
+"""
+class OnlineVariance(object):
+
+    """
+    TODO describe constructor
+    """
     def __init__(self, iterable=None, ddof=1):
         self.ddof, self.n, self.mean, self.M2, self.variance = ddof, 0, 0.0, 0.0, 0.0
         if iterable is not None:
             for datum in iterable:
                 self.include(datum)
 
+    """
+    add a new scalar value as observation to the dataset
+    """
     def include(self, datum):
         self.n += 1
         self.delta = datum - self.mean
